c920586be1e3ffdc7216188ea713d40bf817d12a
2pacalypse <murattopak01@GMAIL.COM>
2018-09-28 22:00:23 +0200

    format

--- RabbitsGrassSimulationModel.java
+++ RabbitsGrassSimulationModel.java
@@ -10,11 +10,9 @@ import uchicago.src.sim.engine.SimInit;
 import uchicago.src.sim.engine.SimModelImpl;
 import uchicago.src.sim.gui.ColorMap;
 import uchicago.src.sim.gui.DisplaySurface;
-import uchicago.src.sim.gui.Displayable;
 import uchicago.src.sim.gui.Object2DDisplay;
-import uchicago.src.sim.gui.SimGraphics;
 import uchicago.src.sim.gui.Value2DDisplay;
-import uchicago.src.sim.space.Discrete2DSpace;
+
 
 /**
  * Class that implements the simulation model for the rabbits grass
@@ -25,297 +23,262 @@ import uchicago.src.sim.space.Discrete2DSpace;
  * @author 
  */
 
+/**
+ * @author Murat
+ *
+ */
+public class RabbitsGrassSimulationModel extends SimModelImpl {
 
-public class RabbitsGrassSimulationModel extends SimModelImpl {	
-	
-		/*
-		 * Initial values.
-		 */
-		private static final int GRIDX = 20;
-		private static final int GRIDY = 20;
-		private static final int NUMRABBITS = 20;
-		private static final int BIRTHTHRESHOLD = 20;
-		private static final int GRASSGROWTHRATE = 20;
-		private static final int RABBITENERGY = 20;
-		private static final int GRASSENERGY = 20;
-		
-		
-		private int gridX = GRIDX;
-		private int gridY = GRIDY;
-		private int numRabbits = NUMRABBITS;
-		
-		private int birthThreshold = BIRTHTHRESHOLD;
-		private int grassGrowthRate = GRASSGROWTHRATE;
-		private int rabbitEnergy = RABBITENERGY;
-		private int grassEnergy = GRASSENERGY;
-		
-		private Schedule schedule;
-		
-		private RabbitsGrassSimulationSpace rgsSpace;
-		
-		private ArrayList<RabbitsGrassSimulationAgent> agentList; 
-		
-		private DisplaySurface displaySurface;
-		
-		private OpenSequenceGraph graph;
-		 
-		
-		class RabbitPopulation implements DataSource, Sequence{
+	private static final int GRIDX = 20;
+	private static final int GRIDY = 20;
+	private static final int NUMRABBITS = 20;
+	private static final int BIRTHTHRESHOLD = 20;
+	private static final int GRASSGROWTHRATE = 20;
+	private static final int RABBITENERGY = 20;
+	private static final int GRASSENERGY = 20;
+
+	private int gridX = GRIDX;
+	private int gridY = GRIDY;
+	private int numRabbits = NUMRABBITS;
+
+	private int birthThreshold = BIRTHTHRESHOLD;
+	private int grassGrowthRate = GRASSGROWTHRATE;
+	private int rabbitEnergy = RABBITENERGY;
+	private int grassEnergy = GRASSENERGY;
+
+	private Schedule schedule;
+
+	private RabbitsGrassSimulationSpace rgsSpace;
+
+	private ArrayList<RabbitsGrassSimulationAgent> agentList;
+
+	private DisplaySurface displaySurface;
+
+	private OpenSequenceGraph graph;
+
+	class RabbitPopulation implements DataSource, Sequence {
+
+		@Override
+		public double getSValue() {
+			return agentList.size();
+		}
+
+		@Override
+		public Object execute() {
+			return Double.valueOf(getSValue());
+		}
+
+	}
+
+	class GrassPopulation implements DataSource, Sequence {
+
+		@Override
+		public double getSValue() {
+			return rgsSpace.getGrassCount();
+		}
+
+		@Override
+		public Object execute() {
+			return Double.valueOf(getSValue());
+		}
+
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Rabbit skeleton");
+		SimInit init = new SimInit();
+		RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
+		init.loadModel(model, "", false);
+	}
+
+	public String getName() {
+		return "EPFL CS430 HW1";
+	}
+
+	public void setup() {
+		System.out.println("Running setup");
+		rgsSpace = null;
+		agentList = new ArrayList<RabbitsGrassSimulationAgent>();
+		schedule = new Schedule(1);
+
+		if (displaySurface != null) {
+			displaySurface.dispose();
+		}
+		displaySurface = null;
+		displaySurface = new DisplaySurface(this, "Model Window");
+		registerDisplaySurface("Model Window", displaySurface);
+
+		if (graph != null) {
+			graph.dispose();
+		}
+		graph = null;
+		graph = new OpenSequenceGraph("Rabbit Population", this);
+		this.registerMediaProducer("Plot", graph);
+
+	}
+
+	public void begin() {
+		buildModel();
+		buildSchedule();
+		buildDisplay();
+
+		displaySurface.display();
+		graph.display();
+	}
+
+	private void buildModel() {
+		System.out.println("Running BuildModel");
+
+		rgsSpace = new RabbitsGrassSimulationSpace(gridX, gridY);
+
+		for (int i = 0; i < numRabbits; i++) {
+			addNewAgent();
+		}
+	}
+
+	private void addNewAgent() {
+		// TODO Auto-generated method stub
+		RabbitsGrassSimulationAgent a = new RabbitsGrassSimulationAgent(rabbitEnergy);
+		agentList.add(a);
+		rgsSpace.addAgent(a);
+	}
+
+	private void buildSchedule() {
+		System.out.println("Running BuildSchedule");
+
+		class RabbitsGrassSimulationStep extends BasicAction {
 
 			@Override
-			public double getSValue() {
-				return agentList.size();
-			}
+			public void execute() {
 
-			@Override
-			public Object execute() {
-				return Double.valueOf(getSValue());
-			}
-			
-		}
-		
-		class GrassPopulation implements DataSource, Sequence{
+				rgsSpace.growGrass(grassGrowthRate);
+				ArrayList<RabbitsGrassSimulationAgent> toBeDeleted = new ArrayList<RabbitsGrassSimulationAgent>();
+				ArrayList<RabbitsGrassSimulationAgent> toBeReproduced = new ArrayList<RabbitsGrassSimulationAgent>();
 
-			@Override
-			public double getSValue() {
-				return rgsSpace.getGrassCount();
-			}
+				for (int i = 0; i < agentList.size(); i++) {
+					RabbitsGrassSimulationAgent rgsAgent = agentList.get(i);
+					rgsAgent.step(grassEnergy);
 
-			@Override
-			public Object execute() {
-				// TODO Auto-generated method stub
-				return Double.valueOf(getSValue());
-			}
-			
-		}
-		 
-		
-		public static void main(String[] args) {
-			System.out.println("Rabbit skeleton");
-			SimInit init = new SimInit();
-			RabbitsGrassSimulationModel model = new RabbitsGrassSimulationModel();
-			init.loadModel(model, "", false);
-		}
-		
-		public String getName() {
-			return "EPFL CS430 HW1";
-		}
-		public void setup() {
-			System.out.println("Running setup");	
-			rgsSpace = null;
-			agentList = new ArrayList<RabbitsGrassSimulationAgent>();
-			schedule = new Schedule(1);
-			
-			if(displaySurface != null) {
-				displaySurface.dispose();
-			}
-			displaySurface = null;
-			displaySurface = new DisplaySurface(this, "Model Window");
-			registerDisplaySurface("Model Window", displaySurface);	
-			
-			
-			if(graph != null) {
-				graph.dispose();
-			}
-			graph = null;
-			graph = new OpenSequenceGraph("Rabbit Population", this);
-			this.registerMediaProducer("Plot", graph);
-			
-			
-		}
-
-		public void begin() {	
-			buildModel();
-		 	buildSchedule();
-		    buildDisplay();
-		    
-		    displaySurface.display();
-		    graph.display();
-		}
-
-		private void buildModel() {
-			System.out.println("Running BuildModel");
-			
-			rgsSpace = new RabbitsGrassSimulationSpace(gridX, gridY);
-			
-			
-			
-			for(int i = 0; i < numRabbits;i++) {
-				addNewAgent();
-			}
-		}
-		
-		private void addNewAgent() {
-			// TODO Auto-generated method stub
-			RabbitsGrassSimulationAgent a = new RabbitsGrassSimulationAgent(rabbitEnergy);
-			agentList.add(a);
-			rgsSpace.addAgent(a);
-		}
-
-		private void buildSchedule() {
-			System.out.println("Running BuildSchedule");
-			
-			
-			
-			class RabbitsGrassSimulationStep extends BasicAction{
-
-				@Override
-				public void execute() {
-					
-					rgsSpace.growGrass(grassGrowthRate);
-					ArrayList<RabbitsGrassSimulationAgent> toBeDeleted = new ArrayList<RabbitsGrassSimulationAgent>();
-					ArrayList<RabbitsGrassSimulationAgent> toBeReproduced = new ArrayList<RabbitsGrassSimulationAgent>();
-						
-					for(int i = 0; i < agentList.size(); i++) {
-						RabbitsGrassSimulationAgent rgsAgent = agentList.get(i);
-						rgsAgent.step(grassEnergy);
-						
-						if(rgsAgent.getEnergy() <= 0) {
-							toBeDeleted.add(rgsAgent);
-						}
-						else if(rgsAgent.getEnergy() >= birthThreshold) {
-							toBeReproduced.add(rgsAgent);
-						}
-						
-
+					if (rgsAgent.getEnergy() <= 0) {
+						toBeDeleted.add(rgsAgent);
+					} else if (rgsAgent.getEnergy() >= birthThreshold) {
+						toBeReproduced.add(rgsAgent);
 					}
-					
-					for(RabbitsGrassSimulationAgent a : toBeDeleted) {
-						agentList.remove(a);
-						rgsSpace.getCurrentAgentSpace().putObjectAt(a.getX(), a.getY(), null);
-					}
-					
-					for(RabbitsGrassSimulationAgent a: toBeReproduced) {
-						a.setEnergy(a.getEnergy() / 2);
-						RabbitsGrassSimulationAgent child = new RabbitsGrassSimulationAgent(rabbitEnergy);
-						if(rgsSpace.addAgent(child)) {
-							agentList.add(child);
-						}
-						
-					}
-					
-					
-					
-					
-					displaySurface.updateDisplay();
+
 				}
-				
-			}
-			schedule.scheduleActionBeginning(0, new RabbitsGrassSimulationStep());
-			
-			class GraphStep extends BasicAction{
 
-				@Override
-				public void execute() {
-					graph.step();
-					
+				for (RabbitsGrassSimulationAgent a : toBeDeleted) {
+					agentList.remove(a);
+					rgsSpace.getCurrentAgentSpace().putObjectAt(a.getX(), a.getY(), null);
 				}
-				
+
+				for (RabbitsGrassSimulationAgent a : toBeReproduced) {
+					a.setEnergy(a.getEnergy() / 2);
+					RabbitsGrassSimulationAgent child = new RabbitsGrassSimulationAgent(rabbitEnergy);
+					if (rgsSpace.addAgent(child)) {
+						agentList.add(child);
+					}
+
+				}
+
+				displaySurface.updateDisplay();
 			}
-			
-			schedule.scheduleActionAtInterval(10, new GraphStep());
+
 		}
-		
-		
-		private void buildDisplay() {
-			System.out.println("Running BuildDisplay");			
-			
-			ColorMap map = new ColorMap();
-			map.mapColor(0, Color.black);
-			map.mapColor(1, Color.green);
-			
-			
-		
-	
-			
-			Value2DDisplay displayGrass = new Value2DDisplay(rgsSpace.getCurrentGrassSpace(), map);
-			
-			Object2DDisplay displayAgents = new Object2DDisplay(rgsSpace.getCurrentAgentSpace());
-		    
-			displayAgents.setObjectList(agentList);
-			
-			
-			displaySurface.addDisplayableProbeable(displayGrass, "Grass");
-			displaySurface.addDisplayableProbeable(displayAgents, "Agents");
-			
-			graph.addSequence("Rabbit Population", new RabbitPopulation(), OpenSequenceGraph.FILLED_CIRCLE);
-			graph.addSequence("Grass Population", new GrassPopulation(), OpenSequenceGraph.FILLED_CIRCLE);
+		schedule.scheduleActionBeginning(0, new RabbitsGrassSimulationStep());
+
+		class GraphStep extends BasicAction {
+			@Override
+			public void execute() {
+				graph.step();
+			}
 		}
+		schedule.scheduleActionAtInterval(10, new GraphStep());
+	}
 
+	private void buildDisplay() {
+		System.out.println("Running BuildDisplay");
 
+		ColorMap map = new ColorMap();
+		map.mapColor(0, Color.black);
+		map.mapColor(1, Color.green);
 
+		Value2DDisplay displayGrass = new Value2DDisplay(rgsSpace.getCurrentGrassSpace(), map);
 
-		public String[] getInitParam() {
-			return new String[] {"GridX", "GridY", "NumRabbits", "BirthThreshold", "GrassGrowthRate", "RabbitEnergy", "GrassEnergy"};
-		}
+		Object2DDisplay displayAgents = new Object2DDisplay(rgsSpace.getCurrentAgentSpace());
 
+		displayAgents.setObjectList(agentList);
 
+		displaySurface.addDisplayableProbeable(displayGrass, "Grass");
+		displaySurface.addDisplayableProbeable(displayAgents, "Agents");
 
-		public Schedule getSchedule() {
-			return schedule;
-		}
-		
-		
+		graph.addSequence("Rabbit Population", new RabbitPopulation(), OpenSequenceGraph.FILLED_CIRCLE);
+		graph.addSequence("Grass Population", new GrassPopulation(), OpenSequenceGraph.FILLED_CIRCLE);
+	}
 
-		public int getGridX() {
-			return gridX;
-		}
+	public String[] getInitParam() {
+		return new String[] { "GridX", "GridY", "NumRabbits", "BirthThreshold", "GrassGrowthRate", "RabbitEnergy",
+				"GrassEnergy" };
+	}
 
-		public void setGridX(int gridX) {
-			this.gridX = gridX;
-		}
+	public Schedule getSchedule() {
+		return schedule;
+	}
 
-		public int getGridY() {
-			return gridY;
-		}
+	public int getGridX() {
+		return gridX;
+	}
 
-		public void setGridY(int gridY) {
-			this.gridY = gridY;
-		}
+	public void setGridX(int gridX) {
+		this.gridX = gridX;
+	}
 
-		public int getNumRabbits() {
-			return numRabbits;
-		}
+	public int getGridY() {
+		return gridY;
+	}
 
-		public void setNumRabbits(int numRabbits) {
-			this.numRabbits = numRabbits;
-		}
+	public void setGridY(int gridY) {
+		this.gridY = gridY;
+	}
 
-		public int getBirthThreshold() {
-			return birthThreshold;
-		}
+	public int getNumRabbits() {
+		return numRabbits;
+	}
 
-		public void setBirthThreshold(int birthThreshold) {
-			this.birthThreshold = birthThreshold;
-		}
+	public void setNumRabbits(int numRabbits) {
+		this.numRabbits = numRabbits;
+	}
 
-		public int getGrassGrowthRate() {
-			return grassGrowthRate;
-		}
+	public int getBirthThreshold() {
+		return birthThreshold;
+	}
 
-		public void setGrassGrowthRate(int grassGrowthRate) {
-			this.grassGrowthRate = grassGrowthRate;
-		}
+	public void setBirthThreshold(int birthThreshold) {
+		this.birthThreshold = birthThreshold;
+	}
 
-		public int getGrassEnergy() {
-			return grassEnergy;
-		}
+	public int getGrassGrowthRate() {
+		return grassGrowthRate;
+	}
 
-		public void setGrassEnergy(int grassEnergy) {
-			this.grassEnergy = grassEnergy;
-		}
-		
-		public int getRabbitEnergy() {
-			return rabbitEnergy;
-		}
+	public void setGrassGrowthRate(int grassGrowthRate) {
+		this.grassGrowthRate = grassGrowthRate;
+	}
 
-		public void setRabbitEnergy(int rabbitEnergy) {
-			this.rabbitEnergy = rabbitEnergy;
-		}
-		
-		
+	public int getGrassEnergy() {
+		return grassEnergy;
+	}
 
+	public void setGrassEnergy(int grassEnergy) {
+		this.grassEnergy = grassEnergy;
+	}
 
-		
+	public int getRabbitEnergy() {
+		return rabbitEnergy;
+	}
+
+	public void setRabbitEnergy(int rabbitEnergy) {
+		this.rabbitEnergy = rabbitEnergy;
+	}
 
 }
--- RabbitsGrassSimulationSpace.java
+++ RabbitsGrassSimulationSpace.java
@@ -1,98 +1,91 @@
-import uchicago.src.sim.space.Discrete2DSpace;
 import uchicago.src.sim.space.Object2DTorus;
 
 /**
  * Class that implements the simulation space of the rabbits grass simulation.
- * @author 
+ * 
+ * @author
  */
 
 public class RabbitsGrassSimulationSpace {
 	private Object2DTorus grassSpace;
 	private Object2DTorus agentSpace;
-	
+
 	private int grassCount = 0;
-	
+
 	public RabbitsGrassSimulationSpace(int xSize, int ySize) {
 		grassSpace = new Object2DTorus(xSize, ySize);
 		agentSpace = new Object2DTorus(xSize, ySize);
-		
+
 		for (int i = 0; i < xSize; i++) {
-			for(int j = 0; j < ySize; j++) {
+			for (int j = 0; j < ySize; j++) {
 				grassSpace.putObjectAt(i, j, Integer.valueOf(0));
 			}
 		}
-		
+
 	}
 
-	
-	
 	public void growGrass(int limit) {
-		for(int i = 0; i < limit; i++) {
+		for (int i = 0; i < limit; i++) {
 			int x = (int) (Math.random() * grassSpace.getSizeX());
 			int y = (int) (Math.random() * grassSpace.getSizeY());
-			if(getGrassAt(x,y) == 0) {
+			if (getGrassAt(x, y) == 0) {
 				grassSpace.putObjectAt(x, y, Integer.valueOf(1));
 				grassCount++;
 			}
-			
+
 		}
 	}
-	
+
 	private int getGrassAt(int x, int y) {
 		// TODO Auto-generated method stub
 		int ret = 0;
-		if(grassSpace.getObjectAt(x, y) != null) {
-			ret =  ((Integer)grassSpace.getObjectAt(x, y)).intValue();
+		if (grassSpace.getObjectAt(x, y) != null) {
+			ret = ((Integer) grassSpace.getObjectAt(x, y)).intValue();
 		}
 		return ret;
 	}
 
 	public boolean isCellOccupied(int x, int y) {
 		boolean retVal = false;
-		if(agentSpace.getObjectAt(x, y) != null) {
+		if (agentSpace.getObjectAt(x, y) != null) {
 			retVal = true;
 		}
 		return retVal;
 	}
-	
+
 	public boolean addAgent(RabbitsGrassSimulationAgent agent) {
 		boolean retVal = false;
 		int count = 0;
 		int countLimit = 10 * agentSpace.getSizeX() * agentSpace.getSizeY();
-		
-		while(retVal == false && count < countLimit) {
+
+		while (retVal == false && count < countLimit) {
 			int x = (int) (Math.random() * agentSpace.getSizeX());
 			int y = (int) (Math.random() * agentSpace.getSizeY());
-			if(isCellOccupied(x, y) == false) {
+			if (isCellOccupied(x, y) == false) {
 				agentSpace.putObjectAt(x, y, agent);
 				agent.setXY(x, y);
-				agent.setRabbitsGrassSimulationSpace(this);
+				agent.setRgsSpace(this);
 				retVal = true;
 			}
 			count++;
 		}
 		return retVal;
-		}
+	}
 
-	
 	public Object2DTorus getCurrentAgentSpace() {
 		return agentSpace;
 	}
-	
+
 	public Object2DTorus getCurrentGrassSpace() {
 		return grassSpace;
 	}
-
-
 
 	public int getGrassCount() {
 		return grassCount;
 	}
 
-
-
 	public void setGrassCount(int grassCount) {
 		this.grassCount = grassCount;
 	}
-	
+
 }
